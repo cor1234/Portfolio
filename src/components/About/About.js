438b57c82fd4b64485826a9eceb898edb7397dd6
Rohan Kumar <rohandbg2019.rk@gmail.com>
2026-08-30 18:07:05 +0530

    Fix CI build warnings from unused imports

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/mj.png";
- import Toolstack from "./Toolstack";
+import Toolstack from "./Toolstack";
 
 function About() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 import { AiFillGithub } from "react-icons/ai";
 // import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
